351bd6ae46c75318fde749cd5c5d24d09128b491
Pablo Murillo <pablomuri@gmail.com>
2025-11-24 09:24:24 +0100

    wip: add config

--- src/RallyConf.cpp
+++ src/RallyConf.cpp
@@ -3,8 +3,23 @@
 
 static const char* BUTTON1_CONF = "button1";
 
-void RallyConf::begin(void)
+void RallyConf::begin()
 {
     preferences.begin(BUTTON1_CONF, false);
+
+    bool tpInit = preferences.isKey("nvsInit");  
+    if (!tpInit) {
+        loadDefaultPref();
+    }
+
+    preferences.end();
+
+
+    
+
+}
+
+void RallyConf::loadDefaultPref() {
+
 }
 
